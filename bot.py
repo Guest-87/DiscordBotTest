da6ccc020952c54088ff1253fcbf86916ac73d7a
Guest-87 <b22457374@gmail.com>
2021-10-25 14:31:39 +0800

    change token ,use json to save token ,add into gitignore

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
+
 import discord
+import json
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix = '[')
-
+input_file = open ('token.json')
+json_token = json.load(input_file)
+for item in json_token :
+    tokens = item['token']
 @bot.event
 async def on_ready():
-    print(">> Bot is online <<")
+    print(">> Bot is online <<") 
 
-bot.run('OTAxODEwMzAxMTk5MDU2OTE2.YXVSSg.fSbFFVOSun6_xxydTl3hR8xC4Nk')
+bot.run(tokens)
